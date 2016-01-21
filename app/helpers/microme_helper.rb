f1b597af2170358475fa15b9b6f7acd99129129f
Admin <mssongan840136@outlook.com>
2016-01-21 14:31:13 +0000

    Finish layout and routes

---
+++ app/helpers/microme_helper.rb
@@ -0,0 +1,2 @@
+module MicromeHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root             'microme#home'
+  get 'help'    => 'microme#help'
+  get 'about'   => 'microme#about'
+  get 'contact' => 'microme#contact'
+  get 'signup'  => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
